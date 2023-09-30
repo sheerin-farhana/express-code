23f58b7565251ccd4d884caea7a545a0ca7e40ae
sheerin-farhana <sheerinfarhana25@gmail.com>
2023-10-01 00:00:53 +0530

    add product route added

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
 
-app.use((req, res, next) => {
-    console.log("In the middleware");
-    next();
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+app.use('/add-product',(req, res, next) => {
+    res.send('<form action="/product" method="post"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add Product</button></form>');
 });
 
-app.use((req, res, next) => {
-    console.log("In another the middleware");
+app.post('/product', (req, res) => {
+    const { title, size } = req.body;
+    console.log(title,size);
+    res.redirect('/');
+});
+
+app.use('/',(req, res, next) => {
+    
     res.send('<h2>Hello from express<h2>');
 });
 
