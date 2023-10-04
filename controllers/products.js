9be29dbde7df5afc686d02477973f59747f296ed
sheerin-farhana <sheerinfarhana25@gmail.com>
2023-10-04 19:54:22 +0530

    models added

--- controllers/products.js
+++ controllers/products.js
@@ -1,4 +1,4 @@
-const products = [];
+const Product = require('../models/product');
 
 const getAddProduct = (req, res, next) => {
     res.render('add-product', {
@@ -12,19 +12,23 @@ const getAddProduct = (req, res, next) => {
 
 
 const postAddProduct = (req, res, next) => {
-    products.push({ title: req.body.title });
+    const product = new Product(req.body.title);
+    product.save();
     res.redirect('/');
 };
 
 const getProducts = (req, res, next) => {
-    res.render('shop', {
-        prods: products,
-        pageTitle: 'Shop',
-        path: '/',
-        hasProducts: products.length > 0,
-        activeShop: true,
-        productCSS: true
-    });
+    Product.fetchAll((products) => {
+        res.render('shop', {
+            prods: products,
+            pageTitle: 'Shop',
+            path: '/',
+            hasProducts: products.length > 0,
+            activeShop: true,
+            productCSS: true
+        });
+   });
+    
 };
 
 
