22bd9a5f4b77f957785eb412ce6080dfb045169d
sheerin-farhana <sheerinfarhana25@gmail.com>
2023-10-04 23:00:40 +0530

    added params

--- routes/admin.js
+++ routes/admin.js
@@ -2,16 +2,17 @@ const path = require('path');
 
 const express = require('express');
 
-const {
-  getAddProduct,
-  postAddProduct } = require('../controllers/products');
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
 // /admin/add-product => GET
-router.get('/add-product', getAddProduct );
+router.get('/add-product', adminController.getAddProduct);
+
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
 
 // /admin/add-product => POST
-router.post('/add-product', postAddProduct );
+router.post('/add-product', adminController.postAddProduct);
 
 module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -2,10 +2,23 @@ const path = require('path');
 
 const express = require('express');
 
-const { getProducts } = require('../controllers/products');
+const shopController = require('../controllers/shop');
+const { route } = require('./admin');
 
 const router = express.Router();
 
-router.get('/', getProducts );
+router.get('/', shopController.getIndex);
+
+router.get('/products', shopController.getProducts);
+
+router.get('/products/:productId',shopController.getProduct);
+
+
+
+router.get('/cart', shopController.getCart);
+
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
